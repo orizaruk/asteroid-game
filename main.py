fa9d6e57667798ad28568da7259653088979832d
orizaruk <oriz1618@gmail.com>
2025-03-27 11:03:07 +0200

    Initialize imports and create the game loop

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        # CLOSE GAME IF USER CLOSED THE SCREEN
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
